55f5522708fd83499f9142be22932fba71d80ec6
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-10-19 16:28:46 +0700

    refactor: remove start

--- push.bat
+++ push.bat
@@ -1,8 +1,8 @@
 RENAME .git .main_git
-start git init
-start git remote add origin https://github.com/Noctissinnit/booking.git
-start git add .
-start git commit -m "Initial Commit"
-start git push origin main
+git init
+git remote add origin https://github.com/Noctissinnit/booking.git
+git add .
+git commit -m "Initial Commit"
+git push origin main
 @RD /S /Q .git
 RENAME .main_git .git
